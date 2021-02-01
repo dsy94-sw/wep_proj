8d33edce96fd5302f61d5909c259c9f060227f2e
wjdgownsll <wjdgownsll@gmail.com>
2021-02-01 16:55:03 +0900

    21.02.01 edit settings.py

--- web_app1/product/models.py
+++ web_app1/product/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Product(models.Model):
     name = models.CharField('NAME', max_length=50)
-
+    
     def __str__(self):
         return self.name
